eab87251f51c6569f337b7b1fa25bb0b3f08217c
Lukasz Pawlik <l.pawlik@selleo.com>
2021-02-11 13:46:44 +0100

    Add readme

---
+++ modules/aws-frontend-app/README.md
@@ -0,0 +1,3 @@
+# AWS Frontend App 
+
+Terraform module which creates CloudFront distribution for frontend application.
--- modules/aws-frontend-app/output.tf
+++ modules/aws-frontend-app/output.tf
@@ -1,11 +1,14 @@
 output "distribution_id" {
-  value = aws_cloudfront_distribution.this.id
+  value       = aws_cloudfront_distribution.this.id
+  description = "CDN distribution ID."
 }
 
 output "domain_name" {
-  value = aws_cloudfront_distribution.this.domain_name
+  value       = aws_cloudfront_distribution.this.domain_name
+  description = "CDN distribution's domain name."
 }
 
 output "hosted_zone_id" {
-  value = aws_cloudfront_distribution.this.hosted_zone_id
+  value       = aws_cloudfront_distribution.this.hosted_zone_id
+  description = "CDN Route 53 zone ID."
 }
